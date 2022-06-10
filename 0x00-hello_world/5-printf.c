e58e3decb2a90af82aa81580e4dd8f5bdeda9349
Anela <anelamade@gmailcom>
2022-06-10 16:01:58 -0700

    My betty commits

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  *   *
  *    * Description: It print a function
  *     *
- *      * Return: 0
- *       */
+ *Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
