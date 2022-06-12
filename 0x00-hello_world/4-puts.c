f5ced73a613d0f394d374cfbebc69859925ac905
Anela <anelamade@gmailcom>
2022-06-12 02:48:30 -0700

    My betty commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - print the string in the puts function
- *   * Description: prints "Programming is like building a multilingual puzzle
- *    * Return: 0   
+ * main - print the string in the puts function
+ * Description: prints "Programming is like building a multilingual puzzle
+ * Return: 0   
  */
 int main(void)
 {
