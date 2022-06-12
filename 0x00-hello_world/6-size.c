d14a7d62ca2000a8d27723f4f8cfdf73795fc7fc
Anela <anelamade@gmailcom>
2022-06-12 03:27:00 -0700

    My betty commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *  *  * main - return integal of 0
- *   *   *
- *    *    * Description: print the size of diffirent characters and words
- *     *     *
- *      *      * Return: Always succes return o
- *       *       *
- *        */
+ * main - return integal of 0
+ * 
+ * Description: print the size of diffirent characters and words
+ *
+ * Return: Always succes return 
+ */
 int main(void)
 {
-			printf("Size of a char: %ld byte(s)\n", sizeof(char));
-						printf("Size of an int: %ld byte(s)\n", sizeof(int));
-										printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-															printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-																					printf("Size of a float: %ld byte(s)\n", sizeof(float));
-																												return (0);
+printf("Size of a char: %ld byte(s)\n", sizeof(char));				
+printf("Size of an int: %ld byte(s)\n", sizeof(int));						printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));																											return (0);
 }
