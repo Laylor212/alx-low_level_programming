77c10463504ea4b34298ec9d968e2ab8233a47a7
Anela <anelamade@gmailcom>
2022-06-13 06:49:24 -0700

    My betty commits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 int d;
 
-for (d = 1; d < 90; d++)
+for (d = 1; d < 100; d++)
 {
-putchar((d / 5) + '0');
-putchar((d % 2) + '0');
-if (d == 89)
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+if (d != 99)
 {
 putchar(',');
 putchar(' ');
