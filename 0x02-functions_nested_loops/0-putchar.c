a6705a2b91611c504f75560dcada1db63f4f75dd
Anela <anelamade@gmailcom>
2022-06-14 00:21:24 -0700

    My betty commits

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,20 @@
-#include <stdio. h>
-int main (){
-printf("_putchar"):
-return 0;
+
+#include "stdio.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+
+return (0);
 }
