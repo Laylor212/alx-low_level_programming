ef91c7c4051757837c35e997dc5033a8c997bdda
Anela <anelamade@gmailcom>
2022-06-16 07:30:59 -0700

    My betty commits

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * @reset_to_98 - takes a pointer to a parameter and updates its value.
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
  * @n: input integer.
  * Return: no return.
  */
