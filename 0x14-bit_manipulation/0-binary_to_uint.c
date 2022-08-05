b5660a8cf536deb1e185904ad279e00fdb5b5d29
Anela <anelamade@gmailcom>
2022-08-05 08:14:52 -0700

    My bit_manipulation commits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,67 @@
-#include <stdio.h>
 #include "main.h"
+#include "stdlib.h"
+/**
+ *  *_pow_recursion - returns the value of x raised to the power of y.
+ *   *@x: parameter integer
+ *    *@y: parameter integer
+ *     *Return: pow number
+ *      */
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+return (_pow_recursion(x, y - 1) * x);
+else if (y == 0)
+return (1);
+else
+return (-1);
+}
 
 /**
- * This function converts a binary number into an unsigned int.
- *
- * Return: Always 0.
- */
-int main(void)
+ *  *_strlen_recursion - returns the length of a string
+ *   *@s: pointer parameter character
+ *    *Return: count size
+ *     */
+int _strlen_recursion(const char *s)
 {
-unsigned int n;
+		int count = 0;
 
-n = binary_to_uint("1");
-printf("%u\n", n);
-n = binary_to_uint("101");
-printf("%u\n", n);
-n = binary_to_uint("1e01");
-printf("%u\n", n);
-n = binary_to_uint("1100010");
-printf("%u\n", n);
-n = binary_to_uint("0000000000000000000110010010");
-printf("%u\n", n);
-return (0);
+			if (*s)
+					{
+								count++;
+										return (count + _strlen_recursion(s + 1));
+											}
+				else
+						{
+									return (count);
+										}
+}
+
+/**
+ *  * binary_to_uint - converts a binary number to an unsigned int
+ *   * @b: is pointing to a string of 0 and 1 chars
+ *    * Return: the converted number
+ *     */
+unsigned int binary_to_uint(const char *b)
+{
+		int i = 0, len = 0;
+			unsigned int number = 0;
+
+				if (!b)
+							return (0);
+
+					len = _strlen_recursion(b) - 1;
+
+						while (b[len])
+								{
+											if (b[len] == '0')
+															number += 0;
+													else if (b[len] == '1')
+																	number += _pow_recursion(2, i);
+															else
+																			return (0);
+																	len--;
+																			i++;
+																				}
+
+						return (number);
+}
